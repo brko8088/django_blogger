d5ff40793ff110aeecdae5d5918cd5456fed277a
Bruno Koppel <brunokoppel.dev@gmail.com>
2021-11-28 09:07:18 -0500

    add registration and login

--- blogger/urls.py
+++ blogger/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogfeed.urls'))
+    path('', include('blogfeed.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from .views import UserRegisterView
 
-urlpatterns = []
+urlpatterns = [
+  path('register/', UserRegisterView.as_view(), name="register"),
+  
+]
