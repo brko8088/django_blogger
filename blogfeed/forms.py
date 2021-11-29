f81da13abdb78906461a11507cfa077bc287a9c2
Bruno Koppel <brunokoppel.dev@gmail.com>
2021-11-29 12:34:58 -0500

    updating registration form

--- blogfeed/forms.py
+++ blogfeed/forms.py
@@ -1,35 +1,37 @@
 from django import forms
 from .models import Category, Post
 
-choices = Category.objects.all().values_list('name','name')
+choices = Category.objects.all().values_list('name', 'name')
 
 choice_list = []
 
 for item in choices:
-  choice_list.append(item)
+    choice_list.append(item)
+
 
 class CreatePostForm(forms.ModelForm):
-  class Meta:
-    model=Post
-    fields=('title', 'title_tag', 'category', 'author', 'body')
-    widgets = {
-      'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder'}),
-      'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-      'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id':'author_field', 'type':'hidden'}),
-      'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': choices}),
-      # 'author': forms.Select(attrs={'class': 'form-control'}),
-      'body': forms.Textarea(attrs={'class': 'form-control'}),
-    }
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'category', 'author', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'author_field', 'type': 'hidden'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': choices}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
 
 class UpdatePostForm(forms.ModelForm):
-  class Meta:
-    model=Post
-    fields=('title', 'title_tag', 'category', 'author', 'body')
-    widgets = {
-      'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder'}),
-      'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-      'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id':'author_field', 'type':'hidden'}),
-      'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': choices}),
-      # 'author': forms.Select(attrs={'class': 'form-control'}),
-      'body': forms.Textarea(attrs={'class': 'form-control'}),
-    }
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'category', 'author', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'author_field', 'type': 'hidden'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': choices}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- members/views.py
+++ members/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegistrationForm
+
 
 class UserRegisterView(generic.CreateView):
-  form_class = UserCreationForm
-  template_name = 'registration/register.html'
-  success_url = reverse_lazy('login')
+    form_class = RegistrationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
 
 # class UserLoginView(generic.)
