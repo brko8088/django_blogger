928c211193e73519101ff06454e7322c22ee6214
Bruno Koppel <brunokoppel.dev@gmail.com>
2021-11-29 18:07:29 -0500

    new stuff

--- blogger/settings.py
+++ blogger/settings.py
@@ -124,11 +124,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIR = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
