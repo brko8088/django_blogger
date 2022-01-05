1d83f045c3defca90308859e504ff8e14bc48dc0
Bruno Koppel <brunokoppel.dev@gmail.com>
2022-01-05 10:28:55 -0500

    update git, sets, reqs

--- blogger/settings.py
+++ blogger/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%a0r$^z%7u!f_nek8cp^=*b#)x#i@1@=ihn4s1)ns*0c-ox#a'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
